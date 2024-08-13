079268370cac800c8c19aa425246b170cc8d6ef1
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-08-13 14:23:57 +0100

    Add files via upload

---
+++ formsubmit.php
@@ -0,0 +1,27 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');
+     
+    $conn = new mysqli("localhost","root","","reactusers");
+    ///  $conn = new mysqli("localhost","root","","reactusers");
+     
+    if(mysqli_connect_error()){
+        echo mysqli_connect_error();
+        exit();
+    }
+    else{
+        $name = $_POST['name'];
+        $mobile = $_POST['mobile'];
+        $email = $_POST['email'];
+         
+        $sql = "INSERT INTO reactusers(name,mobile,email) VALUES('$name','$mobile','$email');";
+        $res = mysqli_query($conn, $sql);
+         
+        if($res){
+            echo "Success!";
+        }
+        else{
+            echo "Error!";
+        }
+        $conn->close();
+    }
+?>
